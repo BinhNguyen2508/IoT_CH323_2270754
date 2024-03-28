89682fc2e8a597143027ec1ea281b7254a1be582
BinhNguyen2508 <briannguyen250896@gmail.com>
2024-03-28 16:30:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import cv2  # Install opencv-python
 from Adafruit_IO import MQTTClient
 from imageRegconition import *
+from uart import *
 
 AIO_FEED_IDs = ["button1","button2"]
 AIO_USERNAME = "nbinhsdh222"
@@ -24,6 +25,17 @@ def disconnected(client):
 def message(client , feed_id , payload):
     print("Feed " + feed_id + " nhan du lieu: " + payload)
 
+    if feed_id == "button1":
+        if payload == "0":
+            writeData(1)
+        else:
+            writeData(2)
+    elif feed_id == "button2":
+        if payload == "0":
+            writeData(3)
+        else:
+            writeData(4)
+
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
 client.on_disconnect = disconnected
@@ -41,22 +53,25 @@ random_interval = random_interval_default
 image_detection_interval = image_detection_interval_default
 
 while True:
-    if random_interval <= 0:
-        random_interval = random_interval_default
-        print("Random data is publishing..." )
-        temperature = random.randint(15,60)
-        client.publish("sensor1", temperature)
-        lumen = random.randint(0,100)
-        client.publish("sensor2", lumen)
-        humidity = random.randint(0,100)
-        client.publish("sensor3", humidity)
+    # if random_interval <= 0:
+    #     random_interval = random_interval_default
+    #     print("Random data is publishing..." )
+    #     temperature = random.randint(15,60)
+    #     client.publish("sensor1", temperature)
+    #     lumen = random.randint(0,100)
+    #     client.publish("sensor2", lumen)
+    #     humidity = random.randint(0,100)
+    #     client.publish("sensor3", humidity)
 
-    if image_detection_interval <=0:
-        print("Image data is publising...")
-        image_detection_interval = image_detection_interval_default
-        imageDetectionResult = imageDetection()
-        client.publish("ai", imageDetectionResult)
+    # if image_detection_interval <=0:
+    #     print("Image data is publising...")
+    #     image_detection_interval = image_detection_interval_default
+    #     imageDetectionResult = imageDetection()
+    #     client.publish("ai", imageDetectionResult)
         
+    # random_interval = random_interval - interval_step
+    # image_detection_interval = image_detection_interval - interval_step
+
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
 
@@ -64,7 +79,7 @@ while True:
     if keyboard_input == 27:
         break
         
+    readSerial(client)
     time.sleep(interval_step)
-    random_interval = random_interval - interval_step
-    image_detection_interval = image_detection_interval - interval_step
+
 
